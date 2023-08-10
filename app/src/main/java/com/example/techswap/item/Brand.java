a489e48399bfe90dc0c70aadc30db5ea2d54492f
unknown <ctin678@aucklanduni.ac.nz>
2023-08-10 15:03:22 +1200

    feat: added required Enums

---
+++ app/src/main/java/com/example/techswap/item/Brand.java
@@ -0,0 +1,5 @@
+package com.example.techswap.item;
+
+public enum Brand {
+    Intel, AMD, Nvidia
+}
---
+++ app/src/main/java/com/example/techswap/item/Socket.java
@@ -0,0 +1,5 @@
+package com.example.techswap.item;
+
+public enum Socket {
+    LGA_1700, LGA_1200, AM5, AM4
+}
