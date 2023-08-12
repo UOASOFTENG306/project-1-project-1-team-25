a3c430a01d79f1422dfc124dd181ffc56fdf5f7e
justin <justinhuang2020@gmail.com>
2023-08-12 23:55:04 +1200

    feat/fix: Implement loginUser method for the login button and resolved warnings in activity_login

--- app/src/main/java/com/example/techswap/LoginActivity.java
+++ app/src/main/java/com/example/techswap/LoginActivity.java
@@ -3,9 +3,9 @@ package com.example.techswap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,5 +16,17 @@ public class LoginActivity extends AppCompatActivity {
         EditText usernameInput = (EditText) findViewById(R.id.username_input_view);
         EditText passwordInput = (EditText) findViewById(R.id.password_input_view);
         Button loginButton = (Button) findViewById(R.id.login_button);
+    }
+
+    public void loginUser(View view) {
+        EditText usernameInput = (EditText) findViewById(R.id.username_input_view);
+        String username = String.valueOf(usernameInput.getText());
+        EditText passwordInput = (EditText) findViewById(R.id.password_input_view);
+        String password = String.valueOf(passwordInput.getText());
+
+        // for when the database is created and can be accessed
+//        User user = dbHandler.getUserByUsername(username);
+//        boolean loggedIn = user.login(password);
+
     }
 }
