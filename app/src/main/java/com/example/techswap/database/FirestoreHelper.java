9f53f4818193d2ed40b2034a2f4e69c8c8e08cd1
dcha762 <100822917+dcha762@users.noreply.github.com>
2023-08-19 13:16:22 +1200

    feat: add user to database method

--- app/src/main/java/com/example/techswap/database/FirestoreHelper.java
+++ app/src/main/java/com/example/techswap/database/FirestoreHelper.java
@@ -1,5 +1,8 @@
 package com.example.techswap.database;
 
+import com.example.techswap.user.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FirestoreHelper {
@@ -8,5 +11,10 @@ public class FirestoreHelper {
 
     public FirestoreHelper() {
         database = FirebaseFirestore.getInstance();
+    }
+
+    public Task<Void> addUser(String userId, User user) {
+        DocumentReference docRef = database.collection("users").document(userId);
+        return docRef.set(user);
     }
 }
