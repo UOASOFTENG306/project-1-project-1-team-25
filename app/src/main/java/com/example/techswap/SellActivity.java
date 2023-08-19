0d8fb8ecf4dcf51257f51abbfb69c84d9427eb97
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2023-08-19 17:22:54 +1200

    feat: added categories to spinner

--- app/src/main/java/com/example/techswap/SellActivity.java
+++ app/src/main/java/com/example/techswap/SellActivity.java
@@ -1,8 +1,10 @@
 package com.example.techswap;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -24,6 +26,12 @@ public class SellActivity extends AppCompatActivity {
     private Button removeImageButton;
     private Button listItemButton;
 
+    private Uri testImage;
+
+    // constant to compare
+    // the activity result code
+    private int SELECT_PICTURE = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +48,18 @@ public class SellActivity extends AppCompatActivity {
         removeImageButton = findViewById(R.id.removeImageButton);
         listItemButton = findViewById(R.id.listItemButton);
 
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        categorySpinner.setAdapter(adapter);
+
         logoText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Code to switch to another activity
+                // switch to another activity
                 Intent intent = new Intent(SellActivity.this, MainActivity.class);
                 startActivity(intent);
             }
@@ -52,10 +68,50 @@ public class SellActivity extends AppCompatActivity {
         listItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Code to switch to another activity
+                // switch to another activity
                 Intent intent = new Intent(SellActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
+        addImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+    }
+
+    // this function is triggered when
+    // the Select Image Button is clicked
+    private void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    // IVPreviewImage.setImageURI(selectedImageUri);
+                    testImage = selectedImageUri;
+                }
+            }
+        }
     }
 }
