4bf00e111ce244748b6b7a8ee2c59fe98d5deaf0
dcha762 <100822917+dcha762@users.noreply.github.com>
2023-08-22 20:59:17 +1200

    feat: fetch cart method

--- app/src/main/java/com/example/techswap/fragments/LoginFragment.java
+++ app/src/main/java/com/example/techswap/fragments/LoginFragment.java
@@ -6,7 +6,6 @@ import static android.view.View.VISIBLE;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -18,10 +17,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.techswap.database.DatabaseSetter;
+import com.example.techswap.item.Cart;
 import com.example.techswap.user.User;
 
 import com.example.techswap.MainActivity;
 import com.example.techswap.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Map;
@@ -113,10 +115,12 @@ public class LoginFragment extends Fragment {
 //
                         if (isLoggingIn && task.getResult().exists() && docData.get("password").toString().equals(user.getPassword())) {
                             User.setCurrentUser(user);
+                            fetchCart(user);
                             startActivity(intent);
                         } else if (!isLoggingIn && !task.getResult().exists())   {
                             dbSetter.addUser(user, true);
                             User.setCurrentUser(user);
+                            fetchCart(user);
                             startActivity(intent);
                         } else if (isLoggingIn){
                             displayMessageTextView.setText("Invalid password or username,\n please try again.");
@@ -125,12 +129,19 @@ public class LoginFragment extends Fragment {
                             displayMessageTextView.setText("Username already in use,\n please try a different one.");
                             displayMessageTextView.setVisibility(VISIBLE);
                         }
-//                        Intent intent = new Intent(requireContext(), MainActivity.class);
-//                        startActivity(intent);
 
                     } else {
                         Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
+    }
+
+    private void fetchCart(User user) {
+        FirebaseFirestore.getInstance().collection("cart").document(user.getUsername())
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User.getCurrentUser().setCart(documentSnapshot.toObject(Cart.class));
+            }});
     }
 }
