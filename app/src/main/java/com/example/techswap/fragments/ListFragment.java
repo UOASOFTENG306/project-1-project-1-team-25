37c0860ea63d28cedcc91f377712ed08bb9c384f
dcha762 <100822917+dcha762@users.noreply.github.com>
2023-08-22 19:14:09 +1200

    feat: fetch category items

--- app/src/main/java/com/example/techswap/fragments/ListFragment.java
+++ app/src/main/java/com/example/techswap/fragments/ListFragment.java
@@ -1,6 +1,9 @@
 package com.example.techswap.fragments;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +13,25 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.techswap.R;
 import com.example.techswap.adapters.CarouselAdapter;
+import com.example.techswap.database.DatabaseUtils;
 import com.example.techswap.databinding.FragmentListBinding;
 import com.example.techswap.item.Details;
 import com.example.techswap.item.Item;
 import com.example.techswap.item.categories.CPU;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ListFragment extends Fragment {
 
     private FragmentListBinding binding;
+
+    private final List<Item> itemList = new ArrayList<>();
+
+    private final DatabaseUtils databaseUtils = new DatabaseUtils();
 
     CarouselAdapter carouselAdapter = new CarouselAdapter(CarouselAdapter.CarouselType.LIST_ITEM);
 
@@ -66,6 +74,23 @@ public class ListFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void fetchItems(String categoryName) {
+        FirebaseFirestore.getInstance().collection("items")
+                .whereEqualTo("category", categoryName).orderBy("title").limit(6)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        itemList.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            itemList.add(databaseUtils.mapToItem(document.getData()));
+                        }
+                        setContent(itemList);
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
     }
 
     public void setContent(List<Item> items) {
