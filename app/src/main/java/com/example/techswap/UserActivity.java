cb8d7b1df252383884fe81c129b2c7fe7f920bec
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2023-08-25 21:56:32 +1200

    refactor: fixed UserActivity warnings

--- app/src/main/java/com/example/techswap/UserActivity.java
+++ app/src/main/java/com/example/techswap/UserActivity.java
@@ -2,8 +2,6 @@ package com.example.techswap;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,34 +15,24 @@ import com.example.techswap.user.User;
 
 public class UserActivity extends AppCompatActivity {
 
-    private ImageView userInBack;
-    private TextView usernameView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        userInBack = findViewById(R.id.userInBack);
-        usernameView = findViewById(R.id.username);
-
-
-
-        userInBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Code to switch to another activity
-                Intent intent = new Intent(UserActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.userInBack).setOnClickListener(v -> {
+            // Code to switch to another activity
+            Intent intent = new Intent(UserActivity.this, MainActivity.class);
+            startActivity(intent);
         });
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         Fragment fragment;
+        TextView usernameView = findViewById(R.id.username);
         if (User.getCurrentUser() == null) {
-            usernameView.setText("Sign in");
+            usernameView.setText(R.string.sign_in);
             fragment = new LoginFragment();
         } else {
             usernameView.setText(User.getCurrentUser().getUsername());
