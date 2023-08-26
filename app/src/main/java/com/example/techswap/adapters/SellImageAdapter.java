7bbb3d1273728397c24712143e21aa521242b850
dcha762 <100822917+dcha762@users.noreply.github.com>
2023-08-26 21:08:39 +1200

    docs: javadocs for rest of adapters

--- app/src/main/java/com/example/techswap/adapters/SellImageAdapter.java
+++ app/src/main/java/com/example/techswap/adapters/SellImageAdapter.java
@@ -5,10 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-import com.bumptech.glide.Glide;
 import com.example.techswap.R;
 import com.example.techswap.interfaces.ISellImageAdapter;
 
@@ -16,9 +13,27 @@ import java.util.List;
 
 public class SellImageAdapter extends ImageAdapter implements ISellImageAdapter {
 
+    /**
+     * Constructor for the SellImageAdapter class.
+     * Initializes the adapter with the provided context and a list of image URLs.
+     *
+     * @param context The context associated with the adapter.
+     * @param imageUrlList The list of image URLs to be displayed in the adapter.
+     */
     public SellImageAdapter(Context context, List<String> imageUrlList) {
         super(context, imageUrlList);
     }
+
+    /**
+     * Called when RecyclerView needs a new {@link ImageViewHolder} of the given type to represent an item.
+     * This method inflates the layout for the item view and creates a new {@link ImageViewHolder} instance
+     * to hold the views within the item. It returns the created {@link ImageViewHolder} to be used for
+     * displaying and managing the data of the item.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new {@link ImageViewHolder} that holds an item view of the given view type.
+     */
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -27,6 +42,13 @@ public class SellImageAdapter extends ImageAdapter implements ISellImageAdapter 
         return new ImageViewHolder(itemView);
     }
 
+    /**
+     * Updates the list of image URLs and notifies the adapter of the data change.
+     * This method sets the new list of image URLs for the adapter and calls {@link #notifyDataSetChanged()}
+     * to inform the RecyclerView that the data has been updated and needs to be reloaded.
+     *
+     * @param imageUrlList The updated list of image URLs to be set for the adapter.
+     */
     @SuppressLint("NotifyDataSetChanged")
     public void updateImages(List<String> imageUrlList) {
         super.setImageUrlList(imageUrlList);
--- app/src/main/java/com/example/techswap/adapters/SpecificationAdapter.java
+++ app/src/main/java/com/example/techswap/adapters/SpecificationAdapter.java
@@ -15,11 +15,28 @@ public class SpecificationAdapter extends RecyclerView.Adapter<SpecificationAdap
     private final List<String> specificationList;
     private final List<String> valueList;
 
+    /**
+     * Constructor for the SpecificationAdapter class.
+     * Initializes the adapter with the provided lists of specification and value data.
+     *
+     * @param specificationList The list of specification data to be displayed in the adapter.
+     * @param valueList The list of corresponding value data to be displayed in the adapter.
+     */
     public SpecificationAdapter(List<String> specificationList, List<String> valueList) {
         this.specificationList = specificationList;
         this.valueList = valueList;
     }
 
+    /**
+     * Called when RecyclerView needs a new {@link SpecificationViewHolder} of the given type to represent an item.
+     * This method inflates the layout for the item view and creates a new {@link SpecificationViewHolder} instance
+     * to hold the views within the item. It returns the created {@link SpecificationViewHolder} to be used for
+     * displaying and managing the specification and value data of the item.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new {@link SpecificationViewHolder} that holds an item view of the given view type.
+     */
     @NonNull
     @Override
     public SpecificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -28,6 +45,14 @@ public class SpecificationAdapter extends RecyclerView.Adapter<SpecificationAdap
         return new SpecificationViewHolder(itemView);
     }
 
+    /**
+     * Called by RecyclerView to display the specification and value data at the specified position.
+     * Binds the specification data at the given position to the {@link SpecificationViewHolder},
+     * and binds the corresponding value data using the provided data source.
+     *
+     * @param holder The {@link SpecificationViewHolder} to bind the data to.
+     * @param position The position of the item within the data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull SpecificationViewHolder holder, int position) {
         String specification = specificationList.get(position);
@@ -37,7 +62,14 @@ public class SpecificationAdapter extends RecyclerView.Adapter<SpecificationAdap
         holder.valueText.setText(value);
     }
 
-
+    /**
+     * Returns the total number of specification-value pairs in the adapter's data set.
+     * If the first specification is empty, it indicates that there is no data to display,
+     * and the method returns 0. Otherwise, it returns the minimum of the sizes of the
+     * specification and value lists.
+     *
+     * @return The total number of specification-value pairs in the data set, or 0 if there is no data.
+     */
     @Override
     public int getItemCount() {
         if (specificationList.get(0).equals("")) {
@@ -50,6 +82,13 @@ public class SpecificationAdapter extends RecyclerView.Adapter<SpecificationAdap
         final TextView specificationText;
         final TextView valueText;
 
+        /**
+         * Constructor for the ViewHolder used to hold specification-value pairs.
+         * Initializes the ViewHolder with the provided item view and associates the specification
+         * and value views with the corresponding layout views to be used for displaying data.
+         *
+         * @param itemView The view representing an individual item in the RecyclerView.
+         */
         public SpecificationViewHolder(@NonNull View itemView) {
             super(itemView);
             specificationText = itemView.findViewById(R.id.specificationText);
