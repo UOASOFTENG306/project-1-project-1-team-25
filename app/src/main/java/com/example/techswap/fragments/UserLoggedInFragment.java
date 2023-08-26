332a0d2960306a1baf91201da2ac522a118ee582
dcha762 <100822917+dcha762@users.noreply.github.com>
2023-08-26 21:37:07 +1200

    docs: javadocs for user login fragment

--- app/src/main/java/com/example/techswap/fragments/UserLoggedInFragment.java
+++ app/src/main/java/com/example/techswap/fragments/UserLoggedInFragment.java
@@ -17,6 +17,14 @@ public class UserLoggedInFragment extends Fragment {
 
     private FragmentUserLoggedInBinding binding;
 
+    /**
+     * Inflates the layout for the login fragment's UI and initializes UI components and click listeners.
+     *
+     * @param inflater           The LayoutInflater used to inflate the layout.
+     * @param container          The parent ViewGroup for the fragment UI.
+     * @param savedInstanceState A Bundle containing saved state information.
+     * @return The root View of the fragment's UI.
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentUserLoggedInBinding.inflate(inflater, container, false);
@@ -24,6 +32,14 @@ public class UserLoggedInFragment extends Fragment {
         return binding.getRoot();
     }
 
+    /**
+     * Called when the fragment's view has been created and is ready to be populated with UI elements.
+     * Sets up click listeners for the "Sell Item" and "Log Out" buttons within the view.
+     *
+     * @param view The root view of the fragment.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
+     *                           Any data that was previously saved in the state can be obtained from here.
+     */
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
